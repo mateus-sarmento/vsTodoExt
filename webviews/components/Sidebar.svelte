a544d889458b3dd03f30c3d3b5236104b179eea7
Mateus Sarmento <mateus.sarmento@ee.ufcg.edu.br>
2022-01-20 18:56:56 -0300

    Added todo list view in Svelte

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,7 +1,32 @@
-<div>Hello There</div>
+<script lang="ts">
+  let todos: Array<{ text: string; completed: boolean }> = [];
+  let text = "";
+</script>
+
+<form
+  on:submit|preventDefault={() => {
+    todos = [{ text, completed: false }, ...todos];
+    text = "";
+  }}
+>
+  <input bind:value={text} />
+</form>
+
+<ul>
+  {#each todos as todo (todo.text)}
+    <li
+      class:complete={todo.completed}
+      on:click={() => {
+        todo.completed = !todo.completed;
+      }}
+    >
+      {todo.text}
+    </li>
+  {/each}
+</ul>
 
 <style>
-  div {
-    color: pink;
+  .complete {
+    text-decoration: line-through;
   }
 </style>
